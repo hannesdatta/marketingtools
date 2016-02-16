a7aaa8b9b34301edf930729024450de75a721837
H. Datta <hdatta@campus.uvt.nl>
2016-02-16 12:44:39 +0100

    removing unused functions, adding rignore, ...

--- R/attraction_example2.R
+++ R/attraction_example2.R
@@ -1,6 +1,6 @@
-source('attraction.R')
-source('itersur.R')
-source('attraction_data.R')
+#source('attraction.R')
+#source('itersur.R')
+#source('attraction_data.R')
 
 
 # Simulate data
@@ -59,45 +59,6 @@ source('attraction_data.R')
 
 # Implementing the model formula object
 
-source('attraction.R')
-source('itersur.R')
-source('attraction_data.R')
-
-
-# The first describes possible Xs and possible Ys; it is verifying the availability of all Xs and all Ys, and - if necessary - kicks out observations that are not complete.
-# There is no need to specify whether a given model is homogenous or heterogenous; as transformations will only be applied later.
-
-# Data prep 1: verify structure of data set
-#  o List possible base brands
-#  o Verify data availability
-#  o 
-
-# Data prep 2: convert to BB approach
-#  o Explicitly specify how many lagged values are to be included
-
-
-
-# Formula: dependent variable (e.g., a market share) ~ homogenous (e.g., 1, an intercept), and | heterogenous coefficients (dummy variables cannot be done here); the homogenous OR heterogenous component should be able to contain lagged depedent variables.
-
-
-
-require(data.table)
-require(reshape2)
-
-
-
-	
-	# compute market shares
-	mf[, ms := y/sum(y), by = c('index_time')]
-	setorder(mf, index_individ, index_time)
-	
-	
-	# Populate dt
-	dt@X <- as.matrix(rawdata$X)
-	dt@y <- as.numeric(rawdata$y)
-	dt@individ <- as.character(rawdata$index$var)
-	dt@period <- rawdata$index$t
-	validObject(dt)
-	
-	return(data.table(mf))
-	
+#source('attraction.R')
+##source('itersur.R')
+#source('attraction_data.R')
