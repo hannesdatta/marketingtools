31bf9cc74a2e4dbe23afe96bbddbd24583f0ab70
H. Datta <hdatta@campus.uvt.nl>
2016-02-23 15:24:41 +0100

    extending copula function to return NA if it's of length 1

--- R/make_copula.R
+++ R/make_copula.R
@@ -1,4 +1,5 @@
 # Copula transformation function (see Park and Gupta 2012), based on a variable's ECDF
 	make_copula <- function(x) {
+		if (length(unique(x))==1) return(rep(NA, length(x)))
 		return(ifelse(ecdf(x)(x)==1, qnorm(1-.0000001), qnorm(ecdf(x)(x))))
 		}
