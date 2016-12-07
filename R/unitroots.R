96be88c0a615a330049599321b76326279c06cfc
Hannes Datta <hannes@datta-online.de>
2016-12-07 17:17:39 +0100

    speed up ADF unit root testing

--- R/unitroots.R
+++ R/unitroots.R
@@ -3,6 +3,7 @@
 #############################################
 
 library(fUnitRoots) # used to obtain McKinnon's (1996) p-values for Unit Root Tests
+library(data.table) # to use the shift function
 
 # Function to difference a series
 	makediff = function(x, order=1) {
@@ -19,6 +20,90 @@ library(fUnitRoots) # used to obtain McKinnon's (1996) p-values for Unit Root Te
 		c(rep(NA,lags),x)[1:length(x)]
 		}
 
+# Function to conduct UR tests concurrently for up to maxlag lags
+# ADF test equation
+adf.test <- function(X, maxlag = 12, trend = TRUE, season = NULL) {
+	#maxlag = 12 # number of maximum lags when determining "common" size of test (to make sure BICs are computed even for 1 lag for data in which lags 12 are used)
+	#lags = 1:12 # lags to be tested
+	#trend = T
+	##season is not activated
+	if (!is.null(season)) stop('Seasonality not supported yet')
+	
+	# Create variables for ADF test equation:
+	# delta_Y_t = alpha + beta*trend + gamma*y_t-1 + d_1 * delta_y_t-1 + d_2 * delta_y_t-2, etc.
+	# test on coefficient of gamma
+	
+	z_diff = c(NA,diff(X))
+	lag_matrix = matrix(unlist(shift(z_diff,1:maxlag)),ncol=maxlag)
+	z_lag_1 = shift(X, 1)
+
+	# Determine first and last observation in data
+	non_na = which(!is.na(X))
+	first_nonna = non_na[1]
+	last_nonna = rev(non_na)[1]
+
+	# Determine observations to estimate models on (i.e., all lags definining max lag are taken out)
+	subs = seq(from = first_nonna + maxlag + 1, to = last_nonna)
+
+	# Add linear trend, if necessary
+	lintrend = NULL
+	if (trend==T) lintrend = subs-min(subs)+1
+
+	# Run up to 0 to maxlag regressions, and save bic to determine selected lag length
+	y = z_diff[subs]
+	n = length(y)
+
+	bics = sapply(0:maxlag, function(lags) {
+		.seq=seq(to=ncol(lag_matrix)-(maxlag-lags), length.out=lags)
+		df = as.matrix(cbind(lag1 = z_lag_1[subs], intercept=1, lintrend = lintrend, lag_matrix[subs, .seq]))
+		XprimeXinv = solve(t(df)%*%df)
+		beta=XprimeXinv%*%(t(df)%*%y)
+		resid = y-df%*%beta
+		k = ncol(df)
+		bic = n * log(sum(resid^2)/n) + (k * log(n))
+		})
+
+	# Define selected lag
+	sellag = which(bics==min(bics))-1
+
+	# Re-estimate final model on all possible observations
+	subs = seq(from = first_nonna + sellag + 1, to = last_nonna)
+	lintrend = NULL
+	if (trend==T) lintrend = subs-min(subs)+1
+
+	y = z_diff[subs]
+	n = length(y)
+
+	.seq=seq(to=ncol(lag_matrix)-(maxlag-sellag), length.out=sellag)
+	df = as.matrix(cbind(lag1 = z_lag_1[subs], intercept=1, lintrend = lintrend, lag_matrix[subs, .seq]))
+			
+	XprimeXinv = solve(t(df)%*%df)
+	beta=XprimeXinv%*%(t(df)%*%y)
+	rownames(beta)[which(rownames(beta)=='')] <- paste0('lag_delta_t-', seq(1, length.out = sellag))
+	
+	resid = y-df%*%beta
+	k = ncol(df)
+	varcovar = 1/(n-k) * drop(t(resid)%*%resid) * XprimeXinv
+	se = sqrt(diag(varcovar))
+	
+	tstat=beta[1]/se[1]
+	if(trend==T) trendspec = 'ct' else trendspec ='c'
+	pval = fUnitRoots::punitroot(tstat, N=n, trend = trendspec, statistic='t')
+	
+	names(bics) <- paste0('lag_length_', 0:(length(bics)-1))
+	
+	coefs = data.frame(beta=beta, se=se)
+	coefs$t = coefs$beta/coefs$se
+	coefs$p = (1-pnorm(abs(coefs$t)))*2
+	coefs$p = (1-pt(abs(coefs$t), n-k-1))*2
+
+	trend_p = NA
+	if (trend==T) trend_p = coefs[3,4]
+	
+	list(t=tstat, p=pval, trend_p=trend_p, coefs = cbind(beta, se), bics = bics, lags = sellag, n = n, k = k, trend = trend)
+	}
+
+if(0){
 # Function to conduct unit root tests with fixed number of lags (on a sample restricted on observations starting at maxlag, to keep up the 
 	adf.equation <- function(X, lags, maxlag, trend, season) { # restrict estimation to elements 'as if' there were ten lags (used for comparison)
 		z.diff = makediff(X)
@@ -52,7 +137,7 @@ library(fUnitRoots) # used to obtain McKinnon's (1996) p-values for Unit Root Te
 		tstat=sum.m$coefficients["z.lag.1",3]
 		pval = fUnitRoots::punitroot(tstat, N=n, trend = trendspec, statistic='t')
 		
-		list(t=tstat, p = pval,fstatistic = sum.m$fstatistic, SBC = bic, SSR=sum(resid(m)^2),n=n,k=k,m1=m)
+		list(t=tstat, p = pval, fstatistic = sum.m$fstatistic, SBC = bic, SSR=sum(resid(m)^2),n=n,k=k,m1=m)
 		}
 
 	
@@ -70,42 +155,36 @@ library(fUnitRoots) # used to obtain McKinnon's (1996) p-values for Unit Root Te
 		return(out)
 		}
 	
+	
+}
+	
 #adf_enders_single(x$used_value, maxlag= max.lag, pval = pval, season=NULL)
 #		X=x$used_value
 		
 # Conducts ADF test using Elder and Kenny's procedure on X (which can be either differenced, or non-differenced)
 	adf_enders_single <- function(X, maxlag, season, pval) {
-		eq1=adf.test(X, maxlag=maxlag, trend=T,season=season)
-		testpval = eq1$p
+		eq1=adf.test(X, maxlag=maxlag, trend=T, season=season)
 		
 		# Procedure described in Elder and Kennedy: Testing for Unit Roots: What Students Should be Taught?
-		if (testpval<=pval) {  
+		if (eq1$p<=pval) {  
 			# --> rejection of test: no unit root; but: test for trend
 			
 			# next: test for significance of trend
-			if ((summary(eq1$m1)$coefficients["lintrend",4])<=pval) {
+			if (eq1$trend_p<=pval) {
 				# trend significant --> NO UR, and a deterministic trend
-				res = list(trend=1, ur = 0, order = 0,n=eq1$n, t=eq1$t,p=testpval, lags = eq1$lags)
+				res = list(trend=1, ur = 0, order = 0, n=eq1$n, t=eq1$t, p=eq1$p, lags = eq1$lags)
 				} else {
 				# trend insignificant --> NO UR, no deterministic trend
-				res = list(trend=0, ur = 0, order = 0,n=eq1$n, t=eq1$t,  p=testpval, lags = eq1$lags)
+				res = list(trend=0, ur = 0, order = 0,n=eq1$n, t=eq1$t,  p=eq1$p, lags = eq1$lags)
 				}
 			} else {
 			
 			# --> not reject: unit root, but NO deterministic trend (check for higher orders!)
-			res = list(trend=0, ur = 1, order = 1, n=eq1$n, t=eq1$t, p=testpval, lags = eq1$lags)
+			res = list(trend=0, ur = 1, order = 1, n=eq1$n, t=eq1$t, p=eq1$p, lags = eq1$lags)
 			}
 		return(res)
 		}
 
-	#adf_regular <- function(X, maxlag, season=NULL) {
-	#	eq1=adf.test(X, maxlag=maxlag, trend=F,season=season)
-	#	testpval = punitroot(eq1$t, N=eq1$n, trend = 'c', statistic='t')
-	#	return(testpval)
-	#	}
-	#adf_regular<-cmpfun(adf_regular)
-	
-	
 ###########
 ## MAIN  ##
 ###########
@@ -134,8 +213,13 @@ adf_enders <- function(X, ...) {
 		
 	}
 
-	
-	
+	adf.test(X)
+
+
+# Test:
+# adf_enders(X, maxlag = 12, season = NULL, pval = .05)
+
+
 # combine p values, see Verbeke 2004, p. 372
 	#adf_by_brand[, ':=' (N_brands=.N, P=-2*sum(log(p))),by=c('category','country', 'variable')][order(category, country,variable)]
 	#adf_by_brand[, combined_p_val := pchisq(unique(P), df=2*unique(N_brands), lower.tail=FALSE), by=c('category','country', 'variable')]
